ca3a1bd4223a35638d7d2fe6a7954f7f55cb4bc1
Begimai Mukaeva <begimaimukaeva2001@gmail.com>
2023-08-12 18:33:27 +0600

    movie App done

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # neobis_front_cinema
 neobis_front_cinema
+
+<h1>"Movie App"</h1>
+<a href=""></a>
+<h2>Project Description:</h2>
+<p>"Movie App" project is a web application designed for viewing movie information and managing a list of favorite films.
+    The application utilizes an open API from the website https://kinopoiskapiunofficial.tech/ to retrieve movie data.
+    To access the API, you'll need to obtain an API key from the official website.</p>
+<h2>Project Structure:</h2>
+<ul>
+    <li>index.html: The main application page that displays movie lists categorized by sections.</li>
+    <li>favorites.html: The page displaying favorite movies, allowing users to view their list of favorite films.</li>
+    <li>main.js: The JavaScript file containing the application's logic, including API requests, data processing, and management of favorite movies.</li>
+    <li>style.css: The main style file for the main page and movie lists.</li>
+    <li>media.css: The style file for responsive design on different devices.</li>
+    <li>img: A folder containing images used in the project.</li>
+</ul>
+<h2>Installation:</h2>
+<ol>
+    <li>Clone the project repository to your computer: git clone </li>
+</ol>
+<h2>Usage:</h2>
+<ol>
+    <li>Obtain an API key from https://kinopoiskapiunofficial.tech/ and add it to the main.js file.</li>
+    <li>Open the index.html file in your web browser to view the main application page.</li>
+    <li>On the main page, you'll see sections of movies: "Premieres of the Current Month",
+        "Top Anticipated Movies", "Top Rated Movies", and "Digital Releases of the Current Month".</li>
+    <li>Each movie is presented as a card with an image, title, category,
+        rating, and an "Add to Favorites" button. Click the heart button to add the movie to your list of favorites.</li>
+    <li>Navigate to the favorites.html page to view your list of favorite movies.
+        If a movie has already been added to favorites, the heart button will be colored red.</li>
+</ol>
+<h2>Functionality:</h2>
+<ul>
+    <li>Displaying movies in sections: "Premieres of the Current Month", "Top Anticipated Movies",
+        "Top Rated Movies", and "Digital Releases of the Current Month" (10 movies in each section).</li>
+    <li>Ability to save movies to favorites.</li>
+    <li>When clicking the heart icon of a movie, it turns red, and the movie is saved to localStorage.</li>
+    <li>Clicking the red heart icon of a favorite movie turns it white, and the movie is removed from localStorage.</li>
+</ul>
+<h2>Contribution:</h2>
+<p>If you'd like to contribute to the project, you can clone the repository, make changes, and create a Pull Request. 
+    Your contribution will be highly valuable for enhancing the application!</p>
+<p>creator: begimaimukaeva2001@gmail.com :))</p>
--- main.js
+++ main.js
@@ -1,62 +1,195 @@
-const API_KEY = 'aa7693a2-d5af-4e7c-b7e1-d2450736091b';
-const API_URL_POPULAR = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1\n';
-const API_URL_SEARCH = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=';
+document.addEventListener('DOMContentLoaded', () => {
+    // const API_KEY = '97c71b3b-2526-4932-a8a1-413d6aecee86';
+    // const API_KEY = 'aa7693a2-d5af-4e7c-b7e1-d2450736091b';
+    const API_KEY = 'd54208c5-9ec7-4274-92d4-aabbdd4d8580';
+    const API_URL_PREMIERES = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2023&month=AUGUST';
+    const API_URL_EXPECTED = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2023&month=SEPTEMBER';
+    const API_URL_TOP_RATED = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1';
+    const API_URL_DIGITAL_RELEASES = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/releases?year=2023&month=JULY&page=1';
 
-getMovies(API_URL_POPULAR);
+    getMovies(API_URL_PREMIERES, 'premieres');
+    getMovies(API_URL_EXPECTED, 'expected');
+    getMovies(API_URL_TOP_RATED, 'topRated');
+    getMovies(API_URL_DIGITAL_RELEASES, 'digitalReleases');
 
-async function getMovies(url){
-    const response = await fetch(url, {
-        headers: {
-            'Content-Type': 'application/json',
-            'X-API-KEY': API_KEY,
-        },
+    async function getMovies(url, sectionId) {
+        const response = await fetch(url, {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-API-KEY': API_KEY,
+            },
+        });
+        const responseData = await response.json();
+        showMovies(responseData, sectionId);
+              updateAllIconsColor();
+    }
+
+    function getClassByRate(vote) {
+        if (vote >= 7) {
+            return 'green';
+        } else if (vote > 5) {
+            return 'orange';
+        } else {
+            return 'red';
+        }
+    }
+
+    function showMovies(data, sectionId) {
+        const sectionElement = document.getElementById(sectionId);
+        const moviesElement = document.createElement('div');
+        moviesElement.classList.add('movies');
+
+        const dataTypes = ['items', 'films', 'releases'];
+
+        dataTypes.forEach(dataType => {
+            if (data[dataType]) {
+                data[dataType].slice(0, 12).forEach(movie => {
+                    const movieElement = document.createElement('div');
+                    movieElement.classList.add('movie');
+                    movieElement.innerHTML = `
+                        <div class="movie">
+                            <div class="movie__cover-inner">
+                                <img class="movie__cover" src="${movie.posterUrlPreview}" alt="${movie.nameRu}">
+                                <div class="movie__cover--darkened"></div>
+                            </div>
+                            <div class="movie__info">
+                                <div class="movie__title">${movie.nameRu}</div>
+                                <div class="movie__category">${movie.genres.map(genre => genre.genre).slice(0, 3)}</div>
+                                ${movie.rating != null ? `<div class="movie__average movie__average--${getClassByRate(movie.rating)}">${movie.rating.toString().substr(0, 3)}</div>` : ''}
+                                <button class="favorite-icon"  data-movie='${JSON.stringify(movie)}' >
+                                   <img class="icon" src="./img/favourite2.png" alt="">
+                                </button>
+
+                            </div>
+                        </div>
+                    `;
+                    moviesElement.appendChild(movieElement);
+                });
+            }
+        });
+        sectionElement.appendChild(moviesElement);
+    }
+    const moviesContainers = document.querySelectorAll('.movies');
+    moviesContainers.forEach(container => {
+        container.addEventListener('click', function(event) {
+            if (event.target.classList.contains('icon')) {
+                const icon = event.target;
+                const movieElement = icon.closest('.movie');
+                const movieDataString = movieElement.querySelector('.favorite-icon').getAttribute('data-movie');
+
+                if (movieDataString) {
+                    const movieData = JSON.parse(movieDataString);
+                    toggleAndStoreFavorite(movieData);
+                    updateIconColor(icon, movieData);
+                }
+            }
+        });
     });
-    const responseData = await response.json();
-    showMovies(responseData);
-}
+    function toggleAndStoreFavorite(movie) {
+        const favorites = getFavorites();
+        const movieIndex = favorites.findIndex(item => item.nameRu === movie.nameRu);
 
-function getClassByRate(vote) {
-    if (vote >= 7){
-        return 'green';
-    }else if (vote > 5){
-        return 'orange';
-    }else if (vote < 5){
-        return 'red';
-    } else {
+        if (movieIndex !== -1) {
+            favorites.splice(movieIndex, 1);
+        } else {
+            favorites.push(movie);
+        }
+
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+        updateAllIconsColor();
     }
-}
-function showMovies(data) {
-    const moviesElement = document.querySelector('.movies');
-    document.querySelector('.movies').innerHTML = '';
-    data.films.forEach(movie => {
-        const movieElement = document.createElement('div');
-        movieElement.classList.add('movie');
-        movieElement.innerHTML = `
-         <div class="movie">
-          <div class="movie__cover-inner">
-            <img class="movie__cover" src="${movie.posterUrlPreview}" alt="${movie.nameRu}">
-            <div class="movie__cover--darkened"></div>
-          </div>
-          <div class="movie__info">
-            <div class="movie__title">${movie.nameRu}</div>
-            <div class="movie__category">${movie.genres.map((genre) => `${genre.genre}`)}</div>
-            <div class="movie__average movie__average--${getClassByRate(movie.rating)}">${movie.rating}</div>
-          </div>
-        </div>
-        `;
-        moviesElement.appendChild(movieElement);
-    })
-}
-
-const form = document.querySelector('form');
-const search = document.querySelector('.header__search');
-
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
-
-    const apiSearchUrl = `${API_URL_SEARCH}${search.value}`
-    if (search.value){
-        getMovies(apiSearchUrl);
-        search.value = '';
+    function getFavorites() {
+        const favoritesJSON = localStorage.getItem('favorites');
+        return favoritesJSON ? JSON.parse(favoritesJSON) : [];
     }
-})
+
+    function updateIconColor(icon, movieData) {
+        const favorites = getFavorites();
+        const isFavorite = favorites.some(favorite => favorite.nameRu === movieData.nameRu);
+
+        if (isFavorite) {
+            icon.style.backgroundColor = 'red';
+        } else {
+            icon.style.backgroundColor = 'white';
+        }
+    }
+
+    function updateAllIconsColor() {
+        const allIcons = document.querySelectorAll('.icon');
+        const favorites = getFavorites();
+
+        allIcons.forEach(icon => {
+            const movieDataString = icon.parentNode.dataset.movie;
+
+            if (movieDataString) {
+                const movieData = JSON.parse(movieDataString);
+                const iconElement = icon;
+
+                if (favorites.some(favorite => favorite.nameRu === movieData.nameRu)) {
+                    iconElement.style.backgroundColor = 'red';
+                } else {
+                    iconElement.style.backgroundColor = 'white';
+                }
+            }
+        });
+    }
+    updateAllIconsColor();
+
+    const favoriteButtons = document.querySelectorAll('.favorite-icon');
+    favoriteButtons.forEach(button => {
+        button.addEventListener('click', function(event) {
+            const icon = event.target;
+            const movieElement = icon.closest('.movie');
+            const movieDataString = movieElement.dataset.movie;
+
+            if (movieDataString) {
+                const movieData = JSON.parse(movieDataString);
+                toggleAndStoreFavorite(movieData);
+                const iconElement = icon;
+                updateIconColor(iconElement, movieData);
+
+                updateAllIconsColor();
+            }
+        });
+    });
+    function showFavorites() {
+        const favoritesContainer = document.getElementById('favorites');
+        if (!favoritesContainer) {
+            return;
+        }
+        const favorites = getFavorites();
+
+        if (favorites.length === 0) {
+            favoritesContainer.innerHTML = '<p class="not-favorite">У вас пока нет избранных фильмов. Для добавления фильма, перейдите на страницу \<a href="index.html" class="header__logo">Movie App</a>\</p>'
+            return;
+        }
+        favoritesContainer.innerHTML = '';
+
+        favorites.forEach(movie => {
+            const movieElement = document.createElement('div');
+            movieElement.classList.add('movie');
+            movieElement.innerHTML = `
+                <div class="movie__cover-inner">
+                    <img class="movie__cover" src="${movie.posterUrlPreview}" alt="${movie.nameRu}">
+                    <div class="movie__cover--darkened"></div>
+                </div>
+                <div class="movie__info">
+                    <div class="movie__title">${movie.nameRu}</div>
+                    <div class="movie__category">${movie.genres.map(genre => genre.genre).slice(0, 3)}</div>
+                    ${movie.rating != null ? `<div class="movie__average movie__average--${getClassByRate(movie.rating)}">${movie.rating.toString().substr(0, 3)}</div>` : ''}
+                    <button class="favorite-icon" data-movie='${JSON.stringify(movie)}'>
+                        <img class="icon" src="./img/favourite2.png" alt="" style="background-color: red">
+                    </button>
+                </div>
+            `;
+            favoritesContainer.appendChild(movieElement);
+        });
+    }
+    showFavorites();
+});
+
+
+
+
+
+
